ced008883596a41a23fab87b2b1c6332618e7011
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-11-23 16:49:01 -0800

    Removed uneeded tagscontroller and tag views

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,5 @@ Rails.application.routes.draw do
   resources :paragraphs, only: [] do
     resources :notes, except: [:index, :show]
   end
-  resources :tags, only: [:index]
   root to: 'welcome#index'
 end
