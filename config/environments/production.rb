9191eebeb25e76a11321edd548981b4d92cdbdc1
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-12-09 15:29:28 -0800

    Configure production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'cogently.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+
 end
