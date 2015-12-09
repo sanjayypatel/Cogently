8443fba70ab55cade5dfc406b85055c757de2bfc
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-12-09 12:09:29 -0800

    Added Fog gem and replaced application.yml values

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -7,8 +7,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -38,9 +37,9 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
